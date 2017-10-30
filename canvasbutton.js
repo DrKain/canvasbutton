36c7f658d8c4d9aecefe24a2a596f8c23a4cfee9
TryHardHusky <TryHardHusky@users.noreply.github.com>
2017-10-30 18:30:34 +1300

    Initial

---
+++ canvasbutton.js
@@ -0,0 +1,206 @@
+var __buttonGlobal = {};
+var __mouse = {x: 0, y: 0, down: false};
+
+var setMouseTrack = function (__$canvas) {
+    __$canvas.on({
+        mousemove: function (e) {
+            var off = __$canvas.offset();
+            __mouse.x = e.pageX - off.left;
+            __mouse.y = e.pageY - off.top;
+        },
+        mousedown: function () {
+            __mouse.down = true;
+        },
+        mouseup: function () {
+            __mouse.down = false;
+        },
+        touchstart: function () {
+            __mouse.down = true;
+        },
+        touchend: function () {
+            __mouse.down = false;
+        },
+        touchcancel: function () {
+            __mouse.down = false;
+        }
+    });
+};
+
+function Button(ctx, options) {
+    this.SHID = Math.random().toString(36).substr(2, 5);
+    this.debug = false;
+
+    __buttonGlobal[this.SHID] = {mo: false};
+
+    this.vars = {};
+
+    this.style = {
+        mouseOver: false,
+        _disabled : false,
+        _hidden : false,
+
+        text: "My Button",
+        x: 5,
+        y: 5,
+        width: 100,
+        height: 40,
+        hover: {
+            backgroundColor: '#5d8ea2',
+            textColor: '#333'
+        },
+        radius: 5,
+        backgroundColor: 'skyblue',
+        borderColor: "black",
+        borderSize: 2,
+        fontSize: '12px',
+        fontFamily: "Arial",
+        textColor: "#333"
+    };
+
+    this.apply(ctx, options);
+    this.onClicked = function () {};
+}
+
+Button.prototype.style = function(code){
+    // TODO: Convert css input
+};
+
+Button.prototype.hide = function(){ this.style._hidden = true; };
+Button.prototype.show = function(){ this.style._hidden = false; };
+
+Button.prototype.disable = function(){
+    this.style._disabled = true;
+    if(Object.keys(this.style.disabled).length > 0){
+        // TODO: Apply Style
+    }
+};
+
+Button.prototype.enable = function(){
+    this.style._disabled = false;
+    if(Object.keys(this.style.disabled).length > 0){
+        // TODO: Remove Disabled Style
+    }
+};
+
+Button.prototype.apply = function (ctx, options) {
+    for (var item in options) {
+        if (this.style[item]) this.style[item] = options[item];
+    }
+    this.draw(ctx);
+};
+
+Button.prototype._stringVar = function (string) {
+    var reg = new RegExp(/\%(.*?)\%/, 'g');
+    var matches = string.match(reg);
+    if (matches !== null) {
+        for (var i = 0; i < matches.length; i++) {
+            var result = matches[i];
+            var search = result.replace(/%/gi, '');
+            if (typeof this.vars[search] !== "undefined")
+                string = string.replace(result, this.vars[search]);
+        }
+    }
+    return string;
+};
+
+Button.prototype.var = function (key, value) {
+    if (typeof value === "undefined") return this.vars[key];
+    else return this.vars[key] = value;
+};
+
+Button.prototype.getText = function () {
+    return this.style.text;
+};
+
+Button.prototype.setText = function (s) {
+    this.style.text = this._stringVar(s);
+};
+
+Button.prototype.opVar = function (key, method) {
+    if (method === "++") this.vars[key]++;
+    if (method === "--") this.vars[key]--;
+};
+
+Button.prototype.mouseenter = function () {
+    if (this.debug) console.log("Button_" + this.SHID + " => Mouse Enter");
+};
+
+Button.prototype.mouseexit = function () {
+    if (this.debug) console.log("Button_" + this.SHID + " => Mouse Exit");
+};
+
+Button.prototype.clicked = function () {
+    if (this.debug) console.log("Button_" + this.SHID + " was clicked!");
+};
+
+Button.prototype.moveTo = function (x, y) {
+    this.style.x = x;
+    this.style.y = y;
+};
+
+Button.prototype.setVar = function (key, value) {
+    this.vars[key] = value;
+};
+
+Button.prototype.getVar = function (key) {
+    return (typeof this.vars[key] !== "undefined" ? this.vars[key] : undefined);
+};
+
+Button.prototype.get = function (key) {
+    return this.style[key] || null;
+};
+
+Button.prototype.set = function(key, value){
+    return (typeof this.style[key] !== "undefined" ? this.style[key] = value : false);
+};
+
+Button.prototype.update = function (mouse, ctx) {
+    var ov = this.style.mouseOver;
+    if (mouse.x >= this.style.x && mouse.x <= (this.style.x + (this.style.width + (this.style.borderSize))) && mouse.y >= this.style.y && mouse.y <= (this.style.y + (this.style.height + (this.style.borderSize)))) {
+        this.style.mouseOver = true;
+        if (this.style.mouseOver !== ov)
+            this.mouseenter();
+        __buttonGlobal[this.SHID].mo = true;
+    } else {
+        this.style.mouseOver = false;
+        if (this.style.mouseOver !== ov)
+            this.mouseexit();
+        __buttonGlobal[this.SHID].mo = false;
+    }
+    if (Object.keys(__buttonGlobal).filter(function (i) {
+            return __buttonGlobal[i].mo === true;
+        }).length > 0) ctx.canvas.style.cursor = 'pointer';
+    else ctx.canvas.style.cursor = 'default';
+    if (this.style.mouseOver === true && mouse.down === true) {
+        this.clicked();
+        mouse.down = false;
+    }
+    this.draw(ctx);
+};
+
+Button.prototype.draw = function (ctx) {
+    ctx.lineWidth = this.style.borderSize;
+    ctx.strokeStyle = this.style.borderColor;
+    ctx.fillStyle = (this.style.mouseOver ? this.style.hover.backgroundColor : this.style.backgroundColor);
+    ctx.beginPath();
+    ctx.moveTo(this.style.x + this.style.radius, this.style.y);
+    ctx.lineTo(this.style.x + this.style.width - this.style.radius, this.style.y);
+    ctx.quadraticCurveTo(this.style.x + this.style.width, this.style.y, this.style.x + this.style.width, this.style.y + this.style.radius);
+    ctx.lineTo(this.style.x + this.style.width, this.style.y + this.style.height - this.style.radius);
+    ctx.quadraticCurveTo(this.style.x + this.style.width, this.style.y + this.style.height, this.style.x + this.style.width - this.style.radius, this.style.y + this.style.height);
+    ctx.lineTo(this.style.x + this.style.radius, this.style.y + this.style.height);
+    ctx.quadraticCurveTo(this.style.x, this.style.y + this.style.height, this.style.x, this.style.y + this.style.height - this.style.radius);
+    ctx.lineTo(this.style.x, this.style.y + this.style.radius);
+    ctx.quadraticCurveTo(this.style.x, this.style.y, this.style.x + this.style.radius, this.style.y);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+    ctx.font = this.style.fontSize + " " + this.style.fontFamily;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+
+    // TODO: Support all style options
+    ctx.fillStyle = (this.style.mouseOver ? this.style.hover.textColor : this.style.textColor);
+
+    ctx.fillText(this.style.text, this.style.x + (this.style.width / 2), this.style.y + (this.style.height / 2));
+};
